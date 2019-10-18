dda1681baebaf916c880336d1c25a29209ab906b
徐旭 <22476705@qq.com>
2019-10-18 14:36:02 +0800

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,12 @@ gulp.task('minify-html', function() {
     return gulp.src('./public/**/*.html')
         .pipe(htmlclean())
         .pipe(htmlmin({
-            removeComments: true,
+            collapseWhitespace: true, //从字面意思应该可以看出来，清除空格，压缩html，这一条比较重要，作用比较大，引起的改变压缩量也特别大
+            collapseBooleanAttributes: true, //省略布尔属性的值，比如：<input checked="checked"/>,那么设置这个属性后，就会变成 <input checked/>
+            removeComments: true, //清除html中注释的部分
+            removeEmptyAttributes: true, //清除所有的空属性
+            removeScriptTypeAttributes: true, //清除所有script标签中的type="text/javascript"属性。
+            removeStyleLinkTypeAttributes: true, //清楚所有Link标签上的type属性。
             minifyJS: true,
             minifyCSS: true,
             minifyURLs: true,
