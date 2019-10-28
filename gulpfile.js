fc8d690a09332080a33ed410406a16ea8b07c106
徐旭 <22476705@qq.com>
2019-10-28 10:49:35 +0800

    hexo文章写作

--- gulpfile.js
+++ gulpfile.js
@@ -32,7 +32,7 @@ gulp.task('minify-css', function() {
 });
 // 压缩js !代表排除的js
 gulp.task('minify-js', function() {
-    return gulp.src('./public/js/**/*.js')
+    return gulp.src(['./public/js/**/.js','!./public/js/**/*min.js'])
         .pipe(uglify()) //压缩混淆
         .pipe(gulp.dest('./public'));
 });
