bca4cbd1ad93ba7848455c786c318e3bad4b75ee
徐旭 <22476705@qq.com>
2020-05-11 17:12:22 +0800

    友链优化

--- source/js/links-color.js
+++ source/js/links-color.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
-    let nickname, avatar, site, info, li = "";
-    $.getJSON("/links/linklist.json",
+    <!-- 技术大佬们友链 -->
+    $.getJSON("/links/skill-link-list.json",
         function (data) {
+            let nickname, avatar, site, info, li = "";
             for (let i = 0; i < data.length; i++) {
                 nickname = data[i].nickname;
                 avatar = data[i].avatar;
@@ -11,6 +12,36 @@ $(document).ready(function () {
                     '<div class="thumb" style="background-image: url(' + avatar + ');"> ' + '</div>' + '<span class="sitename">' + nickname + '</span>' +
                     '<div class="linkdes">' + info + '</div> ' + '</a>' + '</li>';
             }
-            $("#link-navigation").append(li);
+            $("#link-navigation-skill").append(li);
+        });
+    <!-- 实用链接 -->
+    $.getJSON("/links/practical-link-list.json",
+        function (data) {
+            let nickname, avatar, site, info, li = "";
+            for (let i = 0; i < data.length; i++) {
+                nickname = data[i].nickname;
+                avatar = data[i].avatar;
+                site = data[i].site;
+                info = data[i].info;
+                li += '<li class="card">' + '<a href="' + site + '" title="' + info + '" target="_blank">' +
+                    '<div class="thumb" style="background-image: url(' + avatar + ');"> ' + '</div>' + '<span class="sitename">' + nickname + '</span>' +
+                    '<div class="linkdes">' + info + '</div> ' + '</a>' + '</li>';
+            }
+            $("#link-navigation-practical").append(li);
+        });
+    <!-- 虐狗博主 -->
+    $.getJSON("/links/sweet-link-list.json",
+        function (data) {
+            let nickname, avatar, site, info, li = "";
+            for (let i = 0; i < data.length; i++) {
+                nickname = data[i].nickname;
+                avatar = data[i].avatar;
+                site = data[i].site;
+                info = data[i].info;
+                li += '<li class="card">' + '<a href="' + site + '" title="' + info + '" target="_blank">' +
+                    '<div class="thumb" style="background-image: url(' + avatar + ');"> ' + '</div>' + '<span class="sitename">' + nickname + '</span>' +
+                    '<div class="linkdes">' + info + '</div> ' + '</a>' + '</li>';
+            }
+            $("#link-navigation-sweet").append(li);
         });
 });
