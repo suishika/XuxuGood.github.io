3e4cf3d2d181c1414d1e61104a0a39eb4bbe01cb
徐旭 <22476705@qq.com>
2020-07-20 08:56:57 +0800

    优化

--- source/js/crash_cheat.js
+++ source/js/crash_cheat.js
@@ -4,13 +4,13 @@ $(function () {
   var titleTime;
   document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-      $('[rel="icon"]').attr('href', "../../images/failure.png");
-      $('[rel="shortcut icon"]').attr('href', "../../images/failure.png");
+      $('[rel="icon"]').attr('href', "../../images/failure.webp");
+      $('[rel="shortcut icon"]').attr('href', "../../images/failure.webp");
       document.title = '(つェ⊂) 我藏好了哦~ ';
       clearTimeout(titleTime);
     } else {
-      $('[rel="icon"]').attr('href', "../../images/favicon.png");
-      $('[rel="shortcut icon"]').attr('href', "../../images/favicon.png");
+      $('[rel="icon"]').attr('href', "../../images/favicon.webp");
+      $('[rel="shortcut icon"]').attr('href', "../../images/favicon.webp");
       document.title = 'o(^▽^)o 被你发现啦~ ';
       titleTime = setTimeout(function () {
         document.title = OriginTitile;
