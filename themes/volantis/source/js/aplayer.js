f6992c5ce572a07194f2fbbfaa65a50f17d05dde
徐旭 <22476705@qq.com>
2020-08-01 00:20:08 +0800

    优化

--- themes/volantis/source/js/aplayer.js
+++ themes/volantis/source/js/aplayer.js
@@ -4,6 +4,8 @@ function aplayerToggle() {
     if (ap != undefined) {
         ap.toggle();
     }
+    // 切换播放/暂停按钮
+    togglePlayPauseStyle()
 }
 
 // 上一曲
@@ -13,6 +15,8 @@ function aplayerBackward() {
         ap.skipBack();
         ap.play();
     }
+    // 切换为暂停按钮
+    selectPauseStyle()
 }
 
 // 下一曲
@@ -22,6 +26,8 @@ function aplayerForward() {
         ap.skipForward();
         ap.play();
     }
+    // 切换为暂停按钮
+    selectPauseStyle()
 }
 
 // 调节音量
@@ -29,5 +35,24 @@ function aplayerVolume(percent) {
     let ap = document.querySelector('meting-js').aplayer;
     if (ap != undefined) {
         ap.volume(percent);
+    }
+}
+
+// 播放/暂停 按钮状态样式需要切换
+function togglePlayPauseStyle() {
+    if (document.getElementById('aplay').style.display != 'none') {
+        document.getElementById("aplay").style.display = 'none';
+        document.getElementById("apause").style.display = '';
+    } else {
+        document.getElementById("aplay").style.display = '';
+        document.getElementById("apause").style.display = 'none';
+    }
+}
+
+// 上一曲/下一曲 按钮皆为暂停状态样式
+function selectPauseStyle() {
+    if (document.getElementById('aplay').style.display != 'none') {
+        document.getElementById("aplay").style.display = 'none';
+        document.getElementById("apause").style.display = '';
     }
 }
