adead68d9a72cac663a4e5cdc40cb55766053b4f
徐旭 <22476705@qq.com>
2020-08-02 16:30:53 +0800

    优化

--- source/js/canvas-clock.js
+++ source/js/canvas-clock.js
@@ -148,7 +148,7 @@
         var nums = [];
 
         // 时间字体颜色
-        context.fillStyle = "#005eac"
+        context.fillStyle = "#fff"
         var date = new Date();
         var offsetX = 70, offsetY = 30;
         var hours = date.getHours();
