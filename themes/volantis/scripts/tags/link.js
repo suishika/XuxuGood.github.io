37ca725dfabeeb07d0662cb2aacd1635bce6d18b
徐旭 <22476705@qq.com>
2020-08-24 09:46:06 +0800

    优化

--- themes/volantis/scripts/tags/link.js
+++ themes/volantis/scripts/tags/link.js
@@ -3,20 +3,25 @@
 // {% link title, url %}
 // {% link title, url, img %}
 hexo.extend.tag.register('link', function(args) {
-    args = args.join(' ').split(',')
-    let text = ''
-    let url = ''
-    let img = ''
-    if (args.length > 1) {
-        text = args[0].trim()
-        url = args[1].trim()
-    } else {
-        return
-    }
-    if (args.length > 2) {
-        img = args[2].trim()
-        return `<div style="text-align: center"><a class='link-card' title='${url}' href='${url}'><div class='left'><img src=${img}></div><div class='right'><p class='text'>${text}</p><p class='url'>${url}</p></div></a></div>`;
-    } else {
-        return `<div style="text-align: center"><a class='link-card' title='${url}' href='${url}'><div class='left'><i class='fas fa-link'></i></div><div class='right'><p class='text'>${text}</p><p class='url'>${url}</p></div></a></div>`;
-    }
+  args = args.join(' ').split(',')
+  let text = ''
+  let url = ''
+  let img = ''
+  if (args.length > 1) {
+    text = args[0].trim()
+    url = args[1].trim()
+  } else {
+    return
+  }
+  let result = '';
+  result += '<div><a class="link-card" title="' + text + '" href="' + url + '">';
+  // left
+  result += '<div class="left">';
+  result += '<img src="' + (img || hexo.theme.config.tag_plugins.link.placeholder) + '"/>';
+  result += '</div>';
+  // right
+  result += '<div class="right"><p class="text">' + text + '</p><p class="url">' + url + '</p></div>';
+  result += '</a></div>';
+
+  return result;
 });
